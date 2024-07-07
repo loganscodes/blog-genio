0f05a2934957d2a6108fea3c45522291eb5a89dd
Osvaldo Franco <logansafar11@gmail.com>
2024-07-06 21:59:05 -0600

    style: :art: next Script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import { ViewTransitions } from "next-view-transitions";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,13 +20,29 @@ export default function RootLayout({
   return (
     <ViewTransitions>
       <html lang="en">
-        <GoogleAnalytics gaId='G-LK4HDSF5HR'/>
+        <GoogleAnalytics gaId='G-LK4HDSF5HR' />
         <GoogleTagManager gtmId="GTM-W7R8Q5DT" />
         <body className={`${inter.className}`}>
           {children}
           <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W7R8Q5DT"
             height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
-            
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=G-LK4HDSF5HR`}
+          />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-LK4HDSF5HR');
+            `,
+            }}
+          />
+
         </body>
       </html>
 
